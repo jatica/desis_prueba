ac3c494ab6374fe8628deb9df648d1f7cc50ae7e
jatica <prialtplanet@gmail.com>
2024-01-19 10:01:53 -0300

    Update votar.php
    Verificacion por rut

--- votar.php
+++ votar.php
@@ -16,15 +16,23 @@ $tv = $_POST['tv'];
 $redes = $_POST['redes'];
 $amigo = $_POST['amigo'];
 
+// buscar votacion con el mismo rut
 
-// guardando votacion
-$sql = "INSERT INTO votos (nombre, alias, rut, email, region, comuna, candidato, web, tv, redes, amigo) VALUES ('".$nombre."', '".$alias."', '".$rut."', '".$email."', ".$region.", ".$comuna.", ".$candidato.", ".$web.", ".$tv.", ".$redes.", ".$amigo.")";
-if (mysqli_query($coneccion, $sql)) {
-      echo "New record created successfully";
+$resultado_vot = mysqli_query($coneccion, "SELECT * FROM votos WHERE rut LIKE '".$rut."'");
+var_dump($resultado_vot);
+
+if ($resultado_vot->num_rows != 0) {
+      echo "El RUT ya ha votado";
 } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($coneccion);
-}
 
+      // guardando votacion
+      $sql = "INSERT INTO votos (nombre, alias, rut, email, region, comuna, candidato, web, tv, redes, amigo) VALUES ('".$nombre."', '".$alias."', '".$rut."', '".$email."', ".$region.", ".$comuna.", ".$candidato.", ".$web.", ".$tv.", ".$redes.", ".$amigo.")";
+      if (mysqli_query($coneccion, $sql)) {
+            echo "La votación ha sido ingresada";
+      } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($coneccion);
+      }
+}
 
 include 'includes/desconectar.php';
 ?>
